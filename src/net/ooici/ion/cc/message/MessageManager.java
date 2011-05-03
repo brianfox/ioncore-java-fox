3d0ed4c3bdf8cac8af99cde66edbc6df7595aaa8
Brian Fox <bfox@ucsd.edu>
2011-05-03 08:59:01 -0700

    Cleaned up, added documentation.

--- src/net/ooici/ion/cc/message/MessageManager.java
+++ src/net/ooici/ion/cc/message/MessageManager.java
@@ -7,22 +7,48 @@ import org.apache.log4j.Logger;
 import net.ooici.ion.cc.ContainerException;
 import net.ooici.ion.cc.message.stack.MessageStack;
 import net.ooici.ion.cc.message.stack.mailbox.Mailbox;
-import net.ooici.ion.config.LocalProperties;
-import net.ooici.ion.config.PropertiesException;
+import net.ooici.ion.properties.LocalProperties;
+import net.ooici.ion.properties.PropertiesException;
+import net.ooici.util.msgpack.Container;
 
+/**
+ * Provides a single management point for a {@link CapabilityContainer}.  Management 
+ * includes initializing the mesasging system, closing the messaging system, registering
+ * mailboxes, unregistering mailboxes, and logging message related activity.
+ * 
+ * @author brianfox
+ *
+ */
 public class MessageManager {
 
 	private static Logger log = Logger.getLogger(MessageManager.class);
 
 	public static final String CONFIG_SECTION = "MessageManager";
-	ConcurrentHashMap<QosPriority,MessageStack> systems;
 	
+	
+	/*
+	 * There is a one to many relationship between a MessageManager and
+	 * MessageStacks. A single MessageStack establishes basic messaging.
+	 * Multiple MessageStacks lend some interesting configuration options 
+	 * for QoS.
+	 */
+	ConcurrentHashMap<QosPriority,MessageStack> stacks;
+	
+	
+	/**
+	 * 
+	 * @param properties
+	 * @throws ContainerException
+	 * @throws PropertiesException
+	 */
 	public MessageManager(LocalProperties properties) 
 	throws 
 		ContainerException, 
 		PropertiesException
 	{
-		systems = new ConcurrentHashMap<QosPriority,MessageStack>();
+		
+		stacks = new ConcurrentHashMap<QosPriority,MessageStack>();
+		
 		if (properties.containsKey("divisions")) {
 			String[] divisions = properties.getString("divisions").split(",");
 			for (String d : divisions) {
@@ -33,56 +59,48 @@ public class MessageManager {
 					log.error(err);
 					throw new PropertiesException(err);
 				}
-				if (systems.containsKey(q)) {
+				if (stacks.containsKey(q)) {
 					String err = String.format("QosPriority division is already defined: %s", d);
 					log.error(err);
 					throw new PropertiesException(err);
 				}
-				systems.put(QosPriority.fromString(d), new MessageStack(p));
+				stacks.put(QosPriority.fromString(d), new MessageStack(p));
 			}
 		}
+	
 	}
 
 	
+	/**
+	 * 
+	 * @param q
+	 * @param mailbox
+	 * @throws ContainerException
+	 */
 	public void registerMailbox(QosPriority q, Mailbox mailbox) 
 	throws 
 		ContainerException
 	{
-		if (!systems.containsKey(q)) {
+		if (!stacks.containsKey(q)) {
 			String err = String.format("unknown QosPriority: %s", q);
 			log.error(err); 
 			throw new MessageManagerException(err);
 		}
-		systems.get(q).registerMailbox(mailbox);
+		stacks.get(q).registerMailbox(mailbox);
 	}
 	
 	
 	
-	
+	/**
+	 * 
+	 * @throws ContainerException
+	 */
 	public void close() 
 	throws 
 		ContainerException 
 	{
-		for (MessageStack s : systems.values())
+		for (MessageStack s : stacks.values())
 			s.close();
 	}
-
-
-	public MessageStack getMessageSystem(QosPriority priority) 
-	throws 
-		MessageManagerException 
-	{
-		
-		if (!systems.containsKey(priority)) {
-			String err = String.format(
-					"Could not create mailbox, unknown QosPriority: %s", 
-					priority
-			);
-			log.error(err); 
-			throw new MessageManagerException(err);
-		}
-		return systems.get(priority);
-	}
-
 
 }
